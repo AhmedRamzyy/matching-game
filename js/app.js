c6156c5f5c0b92189a73b371d2c0ef325cc6c64c
Ahmed Ramzy <46076630+AhmedRamzyy@users.noreply.github.com>
2019-08-07 16:00:55 +0200

    editing animation time

--- js/app.js
+++ js/app.js
@@ -121,7 +121,7 @@ function unmatched(){
         openedCards[1].classList.remove("show", "open", "no-event","unmatched");
         enable();
         openedCards = [];
-    },1100);
+    },800);
 }
 
 
